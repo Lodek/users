654f43a3b928b0091916d11271b21a73504bc2cf
bruno <br40044422@wipro.com>
2020-02-05 11:22:02 -0200

    refactored user update test

--- src/test/java/com/wipro/bartenders/users/api/user/update/UserUpdateServiceTest.java
+++ src/test/java/com/wipro/bartenders/users/api/user/update/UserUpdateServiceTest.java
@@ -6,25 +6,25 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
-@SpringBootTest
 public class UserUpdateServiceTest {
 
     @Test
-    public void updatingUserShouldOnlyOverwritePropertiesThatAreAvailable(){
-        Long userId = new Long(1);
+    public void updateUser_validUserIdAndUpdatUserObject_ShouldOnlyOverwriteSetProperties(){
+        Long validId = 1L;
         String newEmail = "newemail@email.com";
         new TestSpec()
-        .given_an_existing_user_with_id(userId)
+        .given_an_existing_user_with_id(validId)
         .given_an_user_with_only_email(newEmail)
-        .when_requested_user_update(userId)
-        .then_the_returned_users_email_should_be(newEmail);
+        .when_requested_user_update()
+        .then_the_returned_users_email_should_be_updated();
     }
 }
 
@@ -36,28 +36,31 @@ class TestSpec{
     @Mock
     UserRepository userRepository;
 
+    Long validId;
 
     User existingUser;
     User newUser;
     User updatedUser;
     User returnedUser;
+    private String newEmail;
 
     TestSpec(){
         MockitoAnnotations.initMocks(this);
     }
 
-
-    TestSpec then_the_returned_users_email_should_be(String email) {
-        assertThat(this.returnedUser.getEmail()).isEqualTo(email);
+    TestSpec then_the_returned_users_email_should_be_updated() {
+        assertThat(this.returnedUser.getEmail()).isEqualTo(newEmail);
+        then(userRepository).should().findById(validId);
         return this;
     }
 
-    TestSpec when_requested_user_update(long id) {
-        this.returnedUser = this.service.updateUser(id, this.newUser);
+    TestSpec when_requested_user_update() {
+        this.returnedUser = this.service.updateUser(validId, this.newUser);
         return this;
     }
 
     TestSpec given_an_user_with_only_email(String email) {
+        this.newEmail = email;
         this.newUser = new  User();
         this.newUser.setEmail(email);
         this.updatedUser = new User(existingUser.getId(), existingUser.getUserName(),
@@ -68,9 +71,10 @@ class TestSpec{
     }
 
     TestSpec given_an_existing_user_with_id(long id) {
+        this.validId = id;
         this.existingUser = new User(id, "userName", "firstName", "lastName",
-                LocalDate.parse("01-08-2000"), "email@email.com");
-        given(userRepository.findById(id)).willReturn(java.util.Optional.ofNullable(this.existingUser));
+                LocalDate.parse("2000-01-08"), "email@email.com");
+        given(userRepository.findById(id)).willReturn(Optional.of(this.existingUser));
         return this;
     }
 
